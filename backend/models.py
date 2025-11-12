1494093cf4f0c87c220af85418729caacfa148a1
balbesssss <nonamertrtrt@gmail.com>
2025-11-12 09:18:19 +0300

    update models

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel 
+from pydantic import BaseModel, Field
+from typing import Annotated
 
 
 class Token(BaseModel):
@@ -29,10 +30,11 @@ class GradePutRequest(BaseModel):
     student_full_name: str
     discipline: str
     session: str
-    grade: int
+    grade: int = Field(..., gt=0,le=2)
 
+Grade = Annotated[int, Field(..., ge=2, le=5)]
 
 class MassPutGrades(BaseModel):
     group_name : str
     students: list[str] | None = None
-    grades: list[int] | None = None
+    grades: list[Grade] | None = None
